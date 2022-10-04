fb77a5a67f464dbf39ab679486d132d49c6cd94f
Shivam <shivamseth221104@gmail.com>
2022-10-04 11:02:36 +0530

    Bulb

---
+++ script.js
@@ -0,0 +1,8 @@
+function bulbOn(){
+    let bulbobj =document.querySelector(".bulboff");
+    bulbobj.setAttribute("src" , "on bulb.png")
+}
+function bulbOff(){
+    let bulbobj =document.querySelector(".bulboff");
+    bulbobj.setAttribute("src" , "off bulb.png")
+}
